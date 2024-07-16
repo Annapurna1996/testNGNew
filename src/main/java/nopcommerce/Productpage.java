17fb54e6fdcd02f12888e15903b615a8f382b5a9
Annapurna1996 <annapurnakaja9@gmail.com>
2024-07-16 10:56:07 +0530

    TestNg

---
+++ src/main/java/nopcommerce/Productpage.java
@@ -0,0 +1,80 @@
+package nopcommerce;
+
+import static org.testng.Assert.assertEquals;
+
+import org.apache.commons.math3.stat.descriptive.summary.Product;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Productpage {
+	public Productpage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(xpath="//button[text()='Log in']")
+	WebElement login;
+	//driver.findElement(By.xpath("//a/span[text()='Log in']")).click();
+	public void ClickLogin() {
+		login.click();
+	}
+	@FindBy(xpath="//p[normalize-space()='Catalog']")
+	WebElement Catalog;
+	
+	public void ClickOnCatalog() {
+		Catalog.click();
+	}
+		
+	@FindBy(xpath="//p[normalize-space()='Products']")	
+	  WebElement pro;
+	
+	
+	public void product() {
+		pro.click();
+	}
+	@FindBy(xpath="//i[@class='far fa-angle-up']")
+	WebElement prod;
+	public void ProdClick() {
+		prod.click();
+	}
+	@FindBy(xpath="//input[@id='SearchProductName']")
+	WebElement proName;
+	
+	public void EnterName() {
+		 proName.sendKeys("Portable sound speakers");
+		
+	}
+	@FindBy(xpath="//a[@class='btn btn-primary']")
+	WebElement addprdc;
+	public void AddProduct() {
+		addprdc.click();
+	}
+	@FindBy(xpath="//input[@id='Name']")
+	WebElement EnterProduct;
+	public void AddNewProduct() {
+		EnterProduct.sendKeys("SS");
+		
+	}
+	@FindBy(xpath="//button[@name='save']")
+	WebElement save;
+	public void ClickSave(){
+		save.click();
+	}
+	@FindBy(xpath="//div[@class='alert alert-success alert-dismissable']")
+	WebElement text;
+	public boolean popUp(){
+//		String str = text.getText();
+//		System.out.println("str is:"+str);
+////		if(str.contains("product has been added successfully")) {
+////			System.out.println("Successfully created");
+////		}
+////		else {
+////			System.out.println("Failed to create");
+////		}
+//		//assertEquals(true,str.contains("product has been added successfully"), "Failed to contain text........");
+//		//System.out.println("Sucessfully verified");
+		boolean verifyPopup = text.isDisplayed();
+		return verifyPopup;
+	}
+
+}
---
+++ src/test/java/nopcommerce/ProductTest.java
@@ -0,0 +1,106 @@
+package nopcommerce;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ProductTest {
+	//private static final boolean False = false;
+	//private static final boolean True = false;
+	static WebDriver driver;
+	Productpage pp;
+	@BeforeTest
+	public void setup() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		 
+	}
+	@Test(priority=1)
+	public void clicklogin() {
+		//a/span[text()='Log in']
+		
+		//driver.findElement(By.xpath("//a/span[text()='Log in']")).click();
+		 //Productpage pp = new Productpage(driver);
+		 pp = new Productpage(driver);
+		 pp.ClickLogin();
+		
+	}
+	@Test(priority=2)
+	public void clickCatalog() {
+		pp = new Productpage(driver);
+		pp.ClickOnCatalog();
+		
+	}
+	@Test(priority=3)
+	public void product() {
+       pp = new Productpage(driver);
+		pp.product();
+	}
+	@Test(priority=4)
+	public void clickProArrow() {
+	 pp = new Productpage(driver);
+		pp.ProdClick();
+		
+	}
+	@Test(priority=5)
+	public void EnterName() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Productpage pp = new Productpage(driver);
+		pp.EnterName();
+	}
+	@Test(priority=6)
+	public void AddPRoduct() {
+		 pp = new Productpage(driver);
+		pp.AddProduct();
+		
+	}
+	@Test(priority=7)
+	public void AddNewProduct() {
+		pp = new Productpage(driver);
+		pp.AddNewProduct();
+	}
+	@Test(priority=8)
+	public void Save() {
+     pp = new Productpage(driver);
+		pp.ClickSave();
+		//String text =driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissable']")).getText();
+		//System.out.println("Text is: "+text);
+		//pp.getText();
+		//assertEquals(true,text.contains("product has been added successfully"), "Failed to contain text........");
+		//System.out.println("Sucessfully verified");
+		
+		
+	}
+	@Test(priority=9)
+	public void GetText() {
+		   pp = new Productpage(driver);
+//		     pp.getText();
+//		  assertEquals(true,pp.popUp().contains("product has been added successfully"), "Failed to contain text........");
+//			//System.out.println("Sucessfully verified");
+		   
+		  Assert.assertTrue(pp.popUp());
+		  System.out.println("pop up displayed");
+	}
+	@AfterTest
+	public void close() {
+		driver.close();
+	}
+	
+
+}
